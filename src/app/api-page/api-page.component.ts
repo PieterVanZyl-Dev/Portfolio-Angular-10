b1f62ea20e996752e81a32c6eb9af1dc615c1579
Antitankers <agentpieter@gmail.com>
2020-10-12 10:32:27 +0200

    moved requests to functions to avoid exposing keys

--- src/app/api-page/api-page.component.ts
+++ src/app/api-page/api-page.component.ts
@@ -93,7 +93,6 @@ export class ApiPageComponent implements OnInit {
 
         this.apiService.  SendGetRequestBirdapp().subscribe({
           next: (result: any) => {
-          console.log(result)
           this.BirdAppResponse$ = result;
 
 
